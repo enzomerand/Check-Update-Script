ea85820c625097dad6d11cf10f4e5b7a56f80026
Enzo <Nyzo@users.noreply.github.com>
2015-09-21 21:47:28 +0200

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	    <title><?php echo $setting->name_site; ?></title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="includes/css/<?php echo $cssFile; ?>.css">
+	</head>
+	<body>
